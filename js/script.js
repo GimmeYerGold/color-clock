22f3455ed27c33732a4493c3dd8e52aefd6eb21c
GimmeYerGold <jasongpoland@gmail.com>
2016-02-23 07:57:28 -0600

    added timebar

--- js/script.js
+++ js/script.js
@@ -1,14 +1,47 @@
-var timetext = document.querySelector("h1")
+
+var d = new Date()
+
+var clock = document.querySelector("#clock")
+var color = document.querySelector("#color")
+var background = document.querySelector(".colorOverlay")
+var timebar = document.querySelector("#timeBarProgress")
+
+background.style.backgroundColor = "#" +
+("0" + d.getHours().toString(16)).slice(-2) +
+("0" + d.getMinutes().toString(16)).slice(-2) +
+("0" + d.getSeconds().toString(16)).slice(-2)
+
+clock.textContent = 
+("0" + d.getHours()).slice(-2) + ":" + 
+("0" + d.getMinutes()).slice(-2) + ":" + 
+("0" + d.getSeconds()).slice(-2)
+
+timebar.style.width = parseInt(d.getSeconds()) + "%"
+
+var tickTock = function() {
+
+var d = new Date()
+
+clock.textContent = 
+
+("0" + d.getHours()).slice(-2) + ":" + 
+("0" + d.getMinutes()).slice(-2) + ":" + 
+("0" + d.getSeconds()).slice(-2)
+
+color.textContent =  
+
+("0" + d.getHours().toString(16)).slice(-2) +
+("0" + d.getMinutes().toString(16)).slice(-2) +
+("0" + d.getSeconds().toString(16)).slice(-2) 
+
+background.style.backgroundColor = 
+
+"#" + color.textContent
+
+timebar.style.width = parseInt(d.getSeconds()) + "%"
 
 
-document.querySelector("#time").addEventListener('mouseover',function(){
+}
 
-	timetext.innerHTML = "FFFFFF"
+setInterval(tickTock,1000)
 
-})
-
-document.querySelector("#time").addEventListener('mouseout',function(){
-
-	timetext.innerHTML = "16:59:59"
-
-})
